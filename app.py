71dcc11ea78e77b759c397fd56150a90fff73e46
Gustavo <143751785+gubasssss@users.noreply.github.com>
2023-12-01 08:13:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+mysql = MySQL(app)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form.get('firstname')
+    surname = request.form.get('lastname')
+    email = request.form.get('email')
+    comment = request.form.get('comment')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO contato (name, surname, email, comment) VALUES (%s, %s, %s,%s)', (name, surname, email, comment))
+    mysql.connection.commit()
+    cur.close()
+
+    return redirect(url_for('contact'))
 
 @app.route("/")
 @app.route("/home.html")
@@ -14,7 +34,6 @@ def contact():
 @app.route("/about.html")
 def about():
     return render_template("about.html")
-
 
 
 
